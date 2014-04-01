105a363b2d54c84b89fd24789714dfe23c99d235
山口将平 <shouhei.yamaguci.ht@gmail.com>
2014-04-02 01:58:07 +0900

    change stating for auto-complate

--- init.el
+++ init.el
@@ -105,5 +105,5 @@
 
 ;;;auto-complate
 (require 'auto-complete)
-    (require 'auto-complete-config)
-    (global-auto-complete-mode t)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
