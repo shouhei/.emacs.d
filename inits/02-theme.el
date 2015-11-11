fd2ee50da678d26e44ac4a7b45638f409664edd0
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-11-12 02:09:04 +0900

    delete extra font settings

--- inits/02-theme.el
+++ inits/02-theme.el
@@ -11,16 +11,12 @@
   ;; 英語
   (set-face-attribute 'default nil
                       :family "Ricty Diminished Discord" ;; font
-                      :height 150)    ;; font size
+                      :height 160)    ;; font size
 
   ;; 日本語
   (set-fontset-font
    nil 'japanese-jisx0208
-   (font-spec :family "Ricty Diminished")) ;; font
+   (font-spec :family "Ricty Diminished Discord")) ;; font
 
-  ;; 半角と全角の比を1:2にしたければ
-  (setq face-font-rescale-alist
-        ;;        '((".*Hiragino_Mincho_pro.*" . 1.2)))
-              '((".*Ricty Diminished.*" . 1.2)));; Mac用フォント設定
 )
 (load-theme 'spacemacs-dark t)
