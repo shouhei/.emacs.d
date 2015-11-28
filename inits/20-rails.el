9abe14600f24927354a597e19dbedb6253cce864
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-11-29 02:37:26 +0900

    add ruby conf and delete anything

--- inits/20-rails.el
+++ inits/20-rails.el
@@ -1,5 +1,11 @@
+;; Setting rbenv path
+;; emacs から rbenv でいれた Ruby を利用する.
+(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:"
+             (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
+(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims")
+      (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 (add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
-;(add-to-list 'auto-mode-alist '("\\.erb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.erb$latex " . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$". ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
@@ -35,17 +41,17 @@
      :modes (enh-ruby-mode motion-mode))
 
 ;; definition for flycheck
-(flycheck-define-checker ruby-rubylint
-  "A Ruby syntax and style checker using the rubylint tool."
-  :command ("ruby-lint" source)
-  :error-patterns
-  ((warning line-start
-            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
-            line-end)
-   (error line-start
-          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
-          line-end))
-    :modes (enh-ruby-mode ruby-mode))
+;;(flycheck-define-checker ruby-rubylint
+;;  "A Ruby syntax and style checker using the rubylint tool."
+;;  :command ("ruby-lint" source)
+;;  :error-patterns
+;;  ((warning line-start
+;;            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+;;            line-end)
+;;   (error line-start
+;;          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+;;          line-end))
+;;    :modes (enh-ruby-mode ruby-mode))
 
 (eval-after-load "ruby-mode"
   '(progn
@@ -55,3 +61,7 @@
                          :weight 'semi-bold)))
 
 (add-hook 'ruby-mode-hook 'show-smartparens-mode)
+(require 'rspec-mode)
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
+(autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
