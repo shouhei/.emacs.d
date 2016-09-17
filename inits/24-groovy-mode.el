c38876c01f7b8a613294bbb24110ca72c39cf9a2
山口将平 <shouhei.yamaguchi.be@gmail.com>
2016-09-17 23:28:47 +0900

    fix for groovy

--- inits/24-groovy-mode.el
+++ inits/24-groovy-mode.el
@@ -1,3 +1,4 @@
+(require 'cl)
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
 (add-to-list 'auto-mode-alist '("\.groovy$" . groovy-mode))
 (add-to-list 'interpreter-mode-alist '("grooy" . groovy-mode))
